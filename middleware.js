885c2e989edda0a42f0e724a84f1cd28ef7b89f0
sealdgh <163066861+sealdgh@users.noreply.github.com>
2024-03-11 16:07:43 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = "" // The URL of your Discord/Guilded webhook
+const webhook = "https://discord.com/api/webhooks/1216884877631291502/gORDrIsk9FwrSGOhG51iOD_LpvtLqj_7gL2fCTlCMHWqolLU5sA8tcHFsAr3MkMj0ViY" // The URL of your Discord/Guilded webhook
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
